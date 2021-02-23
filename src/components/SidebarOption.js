0536dcc5c3c3f88962da77be937c865f33be4a33
Thiesen <mailethiesen@gmail.com>
2021-02-23 01:32:28 -0500

    built header and added icons for build out of side bar

---
+++ src/components/SidebarOption.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function SidebarOption() {
+    return <div></div>;
+}
+
+export default SidebarOption;
+
+const SidebarOptionContainer = styled.div``;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: 'AIzaSyBB0VBE2LThvVNQxGPQyNflPJyA2aGezS4',
+    authDomain: 'slack-clone-papa.firebaseapp.com',
+    projectId: 'slack-clone-papa',
+    storageBucket: 'slack-clone-papa.appspot.com',
+    messagingSenderId: '1002808924890',
+    appId: '1:1002808924890:web:84f4a72028ca2472a1a3e3',
+    measurementId: 'G-M3CJFGE5YG',
+};
